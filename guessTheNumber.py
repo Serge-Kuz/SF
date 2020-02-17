3ed0afa30c081009bf37e403a901ae232dba83ce
Kuzmenko <Sergey.Kuzmenko@xerox.com>
2020-02-17 18:51:51 +0300

    удалил лишний код из примера задания

--- guessTheNumber.py
+++ guessTheNumber.py
@@ -1,33 +1,6 @@
 import numpy as np
 import math
 
-def game_core_v1(number):
-    '''Просто угадываем на random ни как не используя информацию о больше или меньше.
-       Функция Принимает загаданное число и возвращает число попыток'''
-    count = 0
-    while True:
-        count+=1
-        predict = np.random.randint(1, 101) # предполагаемое число
-        print ('Predict is {}'.format(predict))
-        if number == predict: 
-            break
-    return(count) # выход из цикла, если угадали
-        
-
-
-def game_core_v2(number):
-    '''Сначала устанавливаем любое random число, а потом уменьшаем или увеличиваем его
-       в зависимости от того, больше оно или меньше нужного.
-       Функция принимает загаданное число и возвращает число попыток'''
-    count = 0
-    predict = np.random.randint(1,100)
-    while number != predict:
-        count+=1
-        if number > predict: 
-            predict += 1
-        elif number < predict: 
-            predict -= 1
-    return(count) # выход из цикла, если угадали
 
 def game_core_v3(number):
     '''Сначала устанавливаем любое random число, а потом уменьшаем или увеличиваем его
